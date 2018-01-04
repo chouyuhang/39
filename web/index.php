e44cf6b0fa74e51c3d9afe165623b916a64c6bc7
chouyuhang <a103410102a@gmail.com>
2018-01-04 13:27:10 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -28,59 +28,32 @@ foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
           $message = $event['message'];
+            $Q2="B1:如何購買「魔法石」？\nB2:「魔法石」可經由什麼付費平台購買呢？\nB3:如果沒有信用卡可怎麼購買「魔法石」？\nB4:為什麼已成功完成交易，但尚未收到「魔法石」？";
             
-            $json = file_get_contents('https://spreadsheets.google.com/feeds/list/2PACX-1vQM1rw7ng4utfYxkEwZ8qAftWD91VEmdlm0XB6eNvRoO3PkJnCTVA9ABFUzPhR7sfKEZup4R15w0T-M/od6/public/values?alt=json');
-            $data = json_decode($json, true);
-            $result = array();
-            foreach ($data['feed']['entry'] as $item) {
-                $keywords = explode(',', $item['gsx$keyword']['$t']);
-                foreach ($keywords as $keyword) {
-                    if (mb_strpos($message['text'], $keyword) !== false) {
-                        $candidate = array(
-                            'thumbnailImageUrl' => $item['gsx$photourl']['$t'],
-                            'title' => $item['gsx$title']['$t'],
-                            'text' => $item['gsx$title']['$t'],
-                            'actions' => array(
-                                array(
-                                'type' => 'sticker',
-                                'packageId' => '1',
-                                'stickerId' => '2',
-                                ),
-                                ),
-                            );
-                        array_push($result, $candidate);
-                    }
-                }
-            }
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $message['text'].'讓我想想喔…',
-                            ),
-                            array(
-                                'type' => 'template',
-                                'altText' => '為您推薦下列美食：',
-                                'template' => array(
-                                    'type' => 'carousel',
-                                    'columns' => $result,
-                                ),
-                            ),
-                            array(
-                                'type' => 'text',
-                                'text' => '這些都超好吃，真心不騙！',
-                            ),
-                            array(
-                                'type' => 'sticker',
-                                'packageId' => '1',
-                                'stickerId' => '2',
-                            ),
-                        ),
-                    ));
-                    break;
+          case ($m_message=="魔法石" ): 
+                            $client->replyMessage(array(
+                           'replyToken' => $event['replyToken'],
+                           'messages' => array(
+                             array(
+                                   'type' => 'text',
+                                   'text' => $B1
+                               )
+                            )
+                        	));              
+                             break;
+            case ($m_message=="魔法石" ): 
+                            $client->replyMessage(array(
+                           'replyToken' => $event['replyToken'],
+                           'messages' => array(
+                             array(
+                                   'type' => 'text',
+                                   'text' => $B2
+                               )
+                            )
+                        	));              
+                             break;
+
+
                 default:
                     error_log("Unsupporeted message type: " . $message['type']);
                     break;
