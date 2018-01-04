1acb0c09d4d51191fe61a7bc0d219c17c9fb17cd
chouyuhang <a103410102a@gmail.com>
2018-01-04 13:42:00 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -38,7 +38,7 @@ foreach ($client->parseEvents() as $event) {
                            'messages' => array(
                              array(
                                    'type' => 'text',
-                                   'text' => $B1
+                                   'text' => $Q2
                                )
                             )
                         	));              
