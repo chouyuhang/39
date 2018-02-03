5120c5f32f4bce587119f844ab4ec84ae5a22cc2
chouyuhang <a103410102a@gmail.com>
2018-02-03 17:33:18 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,32 +3,4 @@ require_once('./LINEBotTiny.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
-foreach ($client->parseEvents() as $event) {
-    switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                
-  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-  "type": "message",
-  "timestamp": 1462629479859,
-  "source": {
-    "type": "user",
-    "userId": "U4af4980629..."
-  },
-  "message": {
-    "id": "325708",
-    "type": "text",
-    "text": "Hello, world!"
-  
-}
-                    
-                    break;
-                
-            }
-            break;
-        default:
-            error_log("Unsupporeted event type: " . $event['type']);
-            break;
-    }
-};
+$bot->sendText($replyToken, "文字訊息");
