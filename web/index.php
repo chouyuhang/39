30b93fba2d437ea867b8438f116158ca29992e32
chouyuhang <a103410102a@gmail.com>
2018-02-03 18:27:37 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,29 +7,6 @@ foreach ($client->parseEvents() as $event) {
 switch ($event['type']) {
         case 'message':
             $message = $event['message'];
-            $json = file_get_contents('https://spreadsheets.google.com/feeds/list/2PACX-1vQM1rw7ng4utfYxkEwZ8qAftWD91VEmdlm0XB6eNvRoO3PkJnCTVA9ABFUzPhR7sfKEZup4R15w0T-M/od6/public/values?alt=json');
-            $data = json_decode($json, true);
-            $result = array();
-            foreach ($data['feed']['entry'] as $item) {
-                $keywords = explode(',', $item['gsx$keyword']['$t']);
-                foreach ($keywords as $keyword) {
-                    if (mb_strpos($message['text'], $keyword) !== false) {
-                        $candidate = array(
-                            'thumbnailImageUrl' => $item['gsx$photourl']['$t'],
-                            'title' => $item['gsx$title']['$t'],
-                            'text' => $item['gsx$title']['$t'],
-                            'actions' => array(
-                                array(
-                                    'type' => 'uri',
-                                    'label' => '查看詳情',
-                                    'uri' => $item['gsx$url']['$t'],
-                                    ),
-                                ),
-                            );
-                        array_push($result, $candidate);
-                    }
-                }
-            }
             switch ($message['type']) {
                 case 'text':
                     $client->replyMessage(array(
@@ -37,26 +14,9 @@ switch ($event['type']) {
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => $message['text'].'讓我想想喔…',
-                            ),
-                            array(
-                                'type' => 'template',
-                                'altText' => '為您推薦下列美食：',
-                                'template' => array(
-                                    'type' => 'carousel',
-                                    'columns' => $result,
-                                ),
-                            ),
-                            array(
-                                'type' => 'text',
-                                'text' => '這些都超好吃，真心不騙！',
-                            ),
-                            array(
-                                'type' => 'sticker',
-                                'packageId' => '1',
-                                'stickerId' => '2',
-                            ),
-                        ),
+                                'text' => $message['text']
+                            )
+                        )
                     ));
                     break;
                 default:
