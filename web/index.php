6f65ce1cdbe485ef6cc043781010045cd082f7e6
chouyuhang <a103410102a@gmail.com>
2018-02-06 20:05:16 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,6 @@
 <?php
-$access_token ='你的Channel Access Token';
+$access_token ='WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU=
+';
 //define('TOKEN', '你的Channel Access Token');
 $json_string = file_get_contents('php://input');
 $json_obj = json_decode($json_string);
