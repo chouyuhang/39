06db9b06232018be4e56a1854fbaa10652314d8e
chouyuhang <a103410102a@gmail.com>
2018-02-06 18:41:26 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,30 +1,31 @@
 <?php
-require_once('./LINEBotTiny.php');
-$channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
-$channelSecret = getenv('LINE_CHANNEL_SECRET');
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-foreach ($client->parseEvents() as $event) {
-switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array( 'type' => 'text',
-                                  'text' => $message['text']
-                            ))));
-                            
-                    break;
-                            
-                default:
-                    error_log("Unsupporeted message type: " . $message['type']);
-                    break;
-            }
-            break;
-        default:
-            error_log("Unsupporeted event type: " . $event['type']);
-            break;
-    }
-};
+$access_token ='WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU=';
+//define('TOKEN', '你的Channel Access Token');
+$json_string = file_get_contents('php://input');
+$json_obj = json_decode($json_string);
+$event = $json_obj->{"events"}[0];
+$type  = $event->{"message"}->{"type"};
+$message = $event->{"message"};
+$reply_token = $event->{"replyToken"};
+$post_data = [
+  "replyToken" => $reply_token,
+  "messages" => [
+    [
+      "type" => "text",
+      "text" => $message->{"text"}
+    ]
+  ]
+];
+$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Authorization: Bearer '.$access_token
+    //'Authorization: Bearer '. TOKEN
+));
+$result = curl_exec($ch);
+curl_close($ch);
+?>
