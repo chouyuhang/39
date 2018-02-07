f6fb9bea1d42f1fe3cdced4c138de71773fceeef
chouyuhang <a103410102a@gmail.com>
2018-02-07 20:00:23 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,59 +1,15 @@
 <?php
-require_once('./LINEBotTiny.php');
-
-$httpClient = new \src\LINEBot\HTTPClient\CurlHTTPClient(getenv('WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU='));
-$bot = new \src\LINEBot($httpClient, ['channelSecret' => getenv('a7e8c58d4744adbc363c42bc558db89e')]);
-$signature = $_SERVER["HTTP_" . \src\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
-$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-foreach ($events as $event) {
-  if ($event instanceof \src\LINEBot\Event\PostbackEvent) {
-    error_log('postback : ' . file_get_contents('php://input')); // date data in postback field
-  }
-
-  if ($event instanceof \src\LINEBot\Event\MessageEvent) {
-    if ($event instanceof \src\LINEBot\Event\MessageEvent\TextMessage) {
-      $json = array(
-        "replyToken" => $event->getReplyToken(),
-        "messages" => [
-          array(
-            "type" => "template",
-            "altText" => "this is a confirm template",
-            "template" => array(
-                "type" => "confirm",
-                "text" => "Are you sure?",
-                "actions" => [
-                    array(
-                      "type" => "datetimepicker",
-                      "data" => "datestring", // will be included in postback action
-                      "label" => "Please Choose",
-                      "mode" => "date", // date | time | datetime
-                      //"initial": "", // 2017-06-18 | 00:00 | 2017-06-18T00:00
-                      //"max": "", // 2017-06-18 | 00:00 | 2017-06-18T00:00
-                      //"min": "", // 2017-06-18 | 00:00 | 2017-06-18T00:00
-                    ),
-                    array(
-                      "type" => "message",
-                      "label" => "No",
-                      "text" => "no"
-                    )
-                ]
-            )
-          )
-        ]
-      );
-
-      $curl = curl_init() ;
-      curl_setopt($curl, CURLOPT_URL, "https://api.line.me/v2/bot/message/reply") ;
-      curl_setopt($curl, CURLOPT_HEADER, true);
-      curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Authorization: Bearer " . getenv('CHANNEL_ACCESS_TOKEN')));
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($json));
-      curl_exec($curl);
-    }
-    continue;
-  }
+$bot = new \LINE\LINEBot(
+  new \LINE\LINEBot\HTTPClient\CurlHTTPClient('WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU='),
+  ['channelSecret' => 'a7e8c58d4744adbc363c42bc558db89e']
+);
+$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$body = file_get_contents("php://input");
+try {
+  $events = $bot->parseEventRequest($body, $signature);
+} catch (Exception $e) {
+  var_dump($e); //錯誤內容
 }
-
- ?>
+$bot->sendText($replyToken, "文字訊息");
+?>
+<?php
