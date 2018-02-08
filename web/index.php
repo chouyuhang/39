25b696994c1b934c8fa7567fa6f8b062bf656f72
chouyuhang <a103410102a@gmail.com>
2018-02-08 19:02:00 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
 $access_token ='WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU=';
 $json_string = file_get_contents('php://input');
+$file = fopen("C:\\Line_log.txt", "a+");
+fwrite($file, $json_string."\n");
 $json_obj = json_decode($json_string);
 $event = $json_obj->{"events"}[0];
 $type  = $event->{"message"}->{"type"};
@@ -15,6 +17,7 @@ $post_data = [
     ]
   ]
 ];
+fwrite($file, json_encode($post_data)."\n");
 $ch = curl_init("https://api.line.me/v2/bot/message/reply");
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
@@ -25,10 +28,12 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json',
     'Authorization: Bearer '.$access_token
-    //'Authorization: Bearer '. TOKEN
 ));
 $result = curl_exec($ch);
+fwrite($file, $result."\n"); 
+fclose($file);
 curl_close($ch);
+
 ?>
 
 
