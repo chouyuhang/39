69626d0525660e2712cf37b7248f0c55a6f894bc
chouyuhang <a103410102a@gmail.com>
2018-02-09 14:25:45 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,7 +1,22 @@
 <?php
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 require_once('./LINEBotTiny.php');
-$channelAccessToken = '<WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU=>';
-$channelSecret = '<a7e8c58d4744adbc363c42bc558db89e>';
+$channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
+$channelSecret = getenv('LINE_CHANNEL_SECRET');
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
@@ -9,19 +24,23 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
-                    $client->replyMessage(array(
+                	$m_message = $message['text'];
+                    $a=$event['source'];
+                    $b=$a['userId'];
+                	if($m_message!="")
+                	{
+                		$client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => $message['text']
+                                'text' => $a . $m_message . $b
                             )
                         )
-                    ));
+                    	));
+                	}
                     break;
-                default:
-                    error_log("Unsupporeted message type: " . $message['type']);
-                    break;
+                
             }
             break;
         default:
