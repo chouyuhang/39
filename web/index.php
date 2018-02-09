00b129218d8104e04c62c8359a42a94da334f7b8
chouyuhang <a103410102a@gmail.com>
2018-02-09 15:45:14 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -22,7 +22,7 @@ foreach ($client->parseEvents() as $event) {
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => "message: ".$m_message."\n"."roomid:".$roomid."\n"."time: ".date('Y-m-d').date("h:i:sa")
+                                'text' => "message: ".$m_message."\n"."roomid:".$roomid."\n"."time: ".date('Y-m-d')." ".date("h:i:sa")
                             ))));
                     }
                 	else if($type=="group")
@@ -32,7 +32,7 @@ foreach ($client->parseEvents() as $event) {
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => "message: ".$m_message."\n"."groupid: ".$groupid."\n"."time: ".date('Y-m-d').date("h:i:sa")
+                                'text' => "message: ".$m_message."\n"."groupid: ".$groupid."\n"."time: ".date('Y-m-d')." ".date("h:i:sa")
                             ))));
                 	}
                     else{
@@ -41,7 +41,7 @@ foreach ($client->parseEvents() as $event) {
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => "message: ".$m_message."\n"."userid: ".$id."\n"."time: ".date('Y-m-d').date("h:i:sa")
+                                'text' => "message: ".$m_message."\n"."userid: ".$id."\n"."time: ".date('Y-m-d')." ".date("h:i:sa")
                             ))));
                     }
                     break;
