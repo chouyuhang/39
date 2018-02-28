6946cd8b468d2f10a6bf0ae48a2f98af5f310241
chouyuhang <a103410102a@gmail.com>
2018-02-28 16:26:38 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,7 +3,8 @@ require_once('./LINEBotTiny.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
-
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'a7e8c58d4744adbc363c42bc558db89e']);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
@@ -12,14 +13,7 @@ foreach ($client->parseEvents() as $event) {
                 case 'text':
                 	$m_message = $message['text']; $source=$event['source']; $idtype = $source['type'];  $id=$source['userId'];
                     $roomid=$source['roomId']; $groupid=$source['groupId']; date_default_timezone_set('Asia/Taipei');
-                    $response = $client->getProfile('Ub28a7054f2aa2bfeeb103fb53ca35f32');
-                    if ($response->isSucceeded()) {
-                     $profile = $response->getJSONDecodedBody();
-                     echo $profile['displayName'];
-                     echo $profile['pictureUrl'];
-                     echo $profile['statusMessage'];
-                    };
-                    else if($m_message=="安安" && $idtype=="room"){
+                    if($m_message=="安安" && $idtype=="room"){
                         $client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
                         'messages' => array(
