978c3d7aa26840fcfbfd5cbd84265c726f96ecd1
chouyuhang <a103410102a@gmail.com>
2018-02-28 15:57:24 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,13 +3,6 @@ use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
-$response = $bot->getProfile('<userId>');
-                    if ($response->isSucceeded()) {
-                     $profile = $response->getJSONDecodedBody();
-                     echo $profile['displayName'];
-                     echo $profile['pictureUrl'];
-                     echo $profile['statusMessage'];
-                    };
 
 
 require_once('./LINEBotTiny.php');
