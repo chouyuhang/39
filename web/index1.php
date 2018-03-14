39531f2d9af10c9ffad9bbd442beee81bb16f9ff
chouyuhang <a103410102a@gmail.com>
2018-03-14 13:56:22 +0800

    Update index1.php

--- web/index1.php
+++ web/index1.php
@@ -1,9 +1,14 @@
 <?php
+require_once('./LINEBotTiny.php');
 require_once __DIR__ . '/../src/LINEBot.php';
+require_once __DIR__ . '/../src/LINEBot/Response.php';
+require_once __DIR__ . '/../src/LINEBot/MessageBuilder.php';
+require_once __DIR__ . '/../src/LINEBot/Constant/MessageType.php';
 require_once __DIR__ . '/../src/LINEBot/Constant/Meta.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient.php';
-require_once __DIR__ . '/../src/LINEBot/MessageBuilder/TextMessageBuilder.php';
+require_once __DIR__ . '/../src/LINEBot/HTTPClient/Curl.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient/CurlHTTPClient.php';
+require_once __DIR__ . '/../src/LINEBot/MessageBuilder/TextMessageBuilder.php';
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
