7c1709a8f04037b9b5a062faca762084307988f2
chouyuhang <a103410102a@gmail.com>
2018-03-14 16:22:05 +0800

    Update index1.php

--- web/index1.php
+++ web/index1.php
@@ -25,13 +25,13 @@ foreach ($client->parseEvents() as $event) {
                     $pictureUrl=$message['pictureUrl'];$res = $bot->getProfile($id);$profile = $res->getJSONDecodedBody();
                     $displayName = $profile['displayName'];
                     date_default_timezone_set('Asia/Taipei');
-                    if($m_message=="安安" && $idtype=="room"){
+                    if($m_message=="安安"){
                         $client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => "姓名:".$displayName."\n"."userid: ".$id.$pictureUrl."\n"."roomid:".$roomid."\n"."time: ".date('Y-m-d h:i:sa')
+                                'text' => "姓名:".$displayName."\n"."userid: ".$id.$pictureUrl."\n"."time: ".date('Y-m-d h:i:sa')
                             ))));
                     }
                     break;
