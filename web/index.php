8474a46bc9f8f4d6bc88591e52c6fe52f67fc7de
chouyuhang <a103410102a@gmail.com>
2018-03-14 16:26:29 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../src/LINEBot.php';
 require_once __DIR__ . '/../src/LINEBot/Response.php';
 require_once __DIR__ . '/../src/LINEBot/Constant/Meta.php';
 require_once __DIR__ . '/../src/LINEBot/MessageBuilder.php';
+require_once __DIR__ . '/../src/LINEBot//Constant/MessageType.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient/Curl.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient/CurlHTTPClient.php';
