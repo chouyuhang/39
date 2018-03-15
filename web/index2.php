3cc40e6dc8e13007d71c8c6aca26e7a48c7ad40b
chouyuhang <a103410102a@gmail.com>
2018-03-15 12:39:37 +0800

    Update index2.php

--- web/index2.php
+++ web/index2.php
@@ -1,7 +1,8 @@
 <?php
-  $str = "Hello World";
-  $file = fopen("abc.txt","a+"); //開啟檔案
-  fwrite($file,$str);
-  fclose($file);
+require_once('./abc.txt');
+$str = "Hello World";
+$file = fopen("abc.txt","a+"); //開啟檔案
+fwrite($file,$str);
+fclose($file);
 
 ?>
