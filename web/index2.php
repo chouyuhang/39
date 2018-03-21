e7fffff273c5a3133ccecf309f9444462b99b787
chouyuhang <a103410102a@gmail.com>
2018-03-21 20:46:11 +0800

    Update index2.php

--- web/index2.php
+++ web/index2.php
@@ -33,7 +33,7 @@ foreach ($client->parseEvents() as $event) {
                     $mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
 			
 			$insert="INSERT INTO mysql (cool,mysqlcol) VALUES ($m_message,$m_message)";
-			$sql = "select mysqlcol from mysql";
+			$sql = "select * from mysql";
 			$result = $mysqli->query($sql);
  
 			while($row = $result->fetch_array(MYSQLI_BOTH)) {
