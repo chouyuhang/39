e8bd96df0dcf3b4ee7d5651814e100e54269a2f0
chouyuhang <a103410102a@gmail.com>
2018-03-24 20:38:36 +0800

    Update index2.php

--- web/index2.php
+++ web/index2.php
@@ -78,7 +78,7 @@ foreach ($client->parseEvents() as $event) {
                     $latitude=$message['latitude'];
                     $longitude=$message['longitude'];
                     if($address!=""){
-                        $msg = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title, $address, $latitude, $longitude);
+                        $msg = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder("位址名稱", "地址", 緯度, 經度);
 			$bot->replyMessage($replyToken,$msg);
                     }
                     break;  
