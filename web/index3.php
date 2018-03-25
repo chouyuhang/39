1264489a1d37658251b5b29e7ceeb5a980d828f6
chouyuhang <a103410102a@gmail.com>
2018-03-25 19:56:21 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -2,13 +2,20 @@
 require_once('./LINEBotTiny.php');
 require_once __DIR__ . '/../src/LINEBot.php';
 require_once __DIR__ . '/../src/LINEBot/Response.php';
-require_once __DIR__ . '/../src/LINEBot/Constant/Meta.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient.php';
-require_once __DIR__ . '/../src/LINEBot/HTTPClient/Curl.php';
-require_once __DIR__ . '/../src/LINEBot/Constant/MessageType.php';
 require_once __DIR__ . '/../src/LINEBot/MessageBuilder.php';
-require_once __DIR__ . '/../src/LINEBot/MessageBuilder/LocationMessageBuilder.php';
+require_once __DIR__ . '/../src/LINEBot/TemplateActionBuilder.php';
+require_once __DIR__ . '/../src/LINEBot/Constant/Meta.php';
+require_once __DIR__ . '/../src/LINEBot/Constant/MessageType.php';
+require_once __DIR__ . '/../src/LINEBot/Constant/ActionType.php';
+require_once __DIR__ . '/../src/LINEBot/Constant/TemplateType.php';
+require_once __DIR__ . '/../src/LINEBot/HTTPClient/Curl.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient/CurlHTTPClient.php';
+require_once __DIR__ . '/../src/LINEBot/TemplateActionBuilder.php';
+require_once __DIR__ . '/../src/LINEBot/TemplateActionBuilder/PostbackTemplateActionBuilder.php';
+require_once __DIR__ . '/../src/LINEBot/MessageBuilder/LocationMessageBuilder.php';
+require_once __DIR__ . '/../src/LINEBot/MessageBuilder/TemplateBuilder.php';
+require_once __DIR__ . '/../src/LINEBot/MessageBuilder/TemplateBuilder/ConfirmTemplateBuilder.php';
 require_once __DIR__ . '/../src/LINEBot/MessageBuilder/TextMessageBuilder.php';
 //USE oqz0qx1hdl6jbtca;
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
@@ -29,27 +36,35 @@ foreach ($client->parseEvents() as $event) {
 		    $address=$message['address']; $title=$message['title'];
                     $longitude=$message['longitude']; $latitude=$message['latitude']; 
                     date_default_timezone_set('Asia/Taipei');$time=date("Y-m-d H:i:s");
-                    if($address!="" || $longitude=="121.605876" || $latitude=="25.07087"){
-			     $mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
-			     $sql="INSERT INTO mysql (name,userid,worktime,worktype,location,longitude,latitude) VALUES ('$displayName','$userid','$time','進','$address','$longitude','$latitude')";
-			     $result = $mysqli->query($sql);
-		    	     $sql = "select * from mysql";
-	            	     $result = $mysqli->query($sql);
- 		    	       while($row = $result->fetch_array(MYSQLI_BOTH)) {
-  		        	 $name = $row['name']; $myid=$row['userid'];
-				 $worktime=$row['worktime']; $worktype=$row['worktype'];
-				 $address=$row['location']; $longitude=$row['longitude'];
-				 $latitude=$row['latitude'];
- 		   	        }
-	            	        if(mysqli_connect_errno()){ 
-                        		$debugmsg='資料庫連線失敗';
-                    	  	}
-                    	  	else{
-			 	        $mysqli->close();
-		    	        }
-                      			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($name." ".$myid." ".$worktime."\n".$worktype."\n".$address."\n".$longitude." ".$latitude);
-			                $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-		                }
+                    //if($address!="" || $longitude=="121.605876" || $latitude=="25.07087"){
+		    if($m_message!=""){
+			/*$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
+			$sql="INSERT INTO mysql (name,userid,worktime,location,longitude,latitude) VALUES ('$displayName','$userid','$time','$address','$longitude','$latitude')";
+			$result = $mysqli->query($sql);
+		    	$sql = "select * from mysql";
+	            	$result = $mysqli->query($sql);
+ 		    	while($row = $result->fetch_array(MYSQLI_BOTH)) {
+  		        	$name = $row['name']; $myid=$row['userid'];
+				$worktime=$row['worktime']; $worktype=$row['worktype'];
+				$address=$row['location']; $longitude=$row['longitude'];
+				$latitude=$row['latitude'];
+ 		   	}
+	            	if(mysqli_connect_errno()){ 
+                        	$debugmsg='資料庫連線失敗';
+                    	}
+                    	else{
+			 	$mysqli->close();
+		    	}*/
+			$actions = array(
+  			new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("是", "ans=Y"),
+  			new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("否", "ans=N")
+			);
+			$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder("問題", $actions);
+			$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $button);
+			$bot->replyMessage($replyToken,$msg);
+                      	//$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($name." ".$myid." ".$worktime."\n".$worktype."\n".$address."\n".$longitude." ".$latitude);
+			//$response = $bot->replyMessage($replyToken, $textMessageBuilder);
+		    }
                     break;
             }
             break;
