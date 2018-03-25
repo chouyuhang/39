138c86921641e1cd91691488b9c3a297ff7b75c1
chouyuhang <a103410102a@gmail.com>
2018-03-25 20:09:25 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -1,14 +1,14 @@
 <?php
 require_once('./LINEBotTiny.php');
 require_once __DIR__ . '/../src/LINEBot.php';
-require_once __DIR__ . '/../src/LINEBot/Response.php';
-require_once __DIR__ . '/../src/LINEBot/HTTPClient.php';
-require_once __DIR__ . '/../src/LINEBot/MessageBuilder.php';
-require_once __DIR__ . '/../src/LINEBot/TemplateActionBuilder.php';
 require_once __DIR__ . '/../src/LINEBot/Constant/Meta.php';
 require_once __DIR__ . '/../src/LINEBot/Constant/MessageType.php';
 require_once __DIR__ . '/../src/LINEBot/Constant/ActionType.php';
 require_once __DIR__ . '/../src/LINEBot/Constant/TemplateType.php';
+require_once __DIR__ . '/../src/LINEBot/Response.php';
+require_once __DIR__ . '/../src/LINEBot/HTTPClient.php';
+require_once __DIR__ . '/../src/LINEBot/MessageBuilder.php';
+require_once __DIR__ . '/../src/LINEBot/TemplateActionBuilder.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient/Curl.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient/CurlHTTPClient.php';
 require_once __DIR__ . '/../src/LINEBot/TemplateActionBuilder.php';
