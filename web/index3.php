b2981be61ae1732bb7e674ed62d346f9675096a6
chouyuhang <a103410102a@gmail.com>
2018-03-25 20:12:16 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -1,14 +1,14 @@
 <?php
 require_once('./LINEBotTiny.php');
 require_once __DIR__ . '/../src/LINEBot.php';
-require_once __DIR__ . '/../src/LINEBot/Constant/Meta.php';
-require_once __DIR__ . '/../src/LINEBot/Constant/MessageType.php';
-require_once __DIR__ . '/../src/LINEBot/Constant/ActionType.php';
-require_once __DIR__ . '/../src/LINEBot/Constant/TemplateType.php';
 require_once __DIR__ . '/../src/LINEBot/Response.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient.php';
 require_once __DIR__ . '/../src/LINEBot/MessageBuilder.php';
 require_once __DIR__ . '/../src/LINEBot/TemplateActionBuilder.php';
+require_once __DIR__ . '/../src/LINEBot/Constant/Meta.php';
+require_once __DIR__ . '/../src/LINEBot/Constant/MessageType.php';
+require_once __DIR__ . '/../src/LINEBot/Constant/ActionType.php';
+require_once __DIR__ . '/../src/LINEBot/Constant/TemplateType.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient/Curl.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient/CurlHTTPClient.php';
 require_once __DIR__ . '/../src/LINEBot/TemplateActionBuilder.php';
@@ -17,6 +17,7 @@ require_once __DIR__ . '/../src/LINEBot/MessageBuilder/TemplateBuilder.php';
 require_once __DIR__ . '/../src/LINEBot/MessageBuilder/LocationMessageBuilder.php';
 require_once __DIR__ . '/../src/LINEBot/MessageBuilder/TemplateBuilder/ConfirmTemplateBuilder.php';
 require_once __DIR__ . '/../src/LINEBot/MessageBuilder/TextMessageBuilder.php';
+require_once __DIR__ . '/../src/LINEBot/MessageBuilder/TemplateMessageBuilder.php';
 //USE oqz0qx1hdl6jbtca;
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
