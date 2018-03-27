9131b73ff6e95ea5c2f09e725a98f789ae62d086
chouyuhang <a103410102a@gmail.com>
2018-03-27 20:20:07 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -83,7 +83,7 @@ foreach ($client->parseEvents() as $event) {
                     date_default_timezone_set('Asia/Taipei');$time=date("Y-m-d H:i:s");
 		    if($m_message=="安安"){
 		    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-		    $response = $bot->pushMessage('Ub28a7054f2aa2bfeeb103fb53ca35f32', $textMessageBuilder);
+		    $response = $bot->pushMessage($userid, $textMessageBuilder);
 		    }    
 		    else if($m_message=="進"){
 			$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
