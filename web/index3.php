3b5ad0dea72f575264f49dd4dd3c8a33a5938dd7
chouyuhang <a103410102a@gmail.com>
2018-03-28 19:19:14 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -81,7 +81,7 @@ foreach ($client->parseEvents() as $event) {
 		    $address=$message['address']; $title=$message['title'];
                     $longitude=$message['longitude']; $latitude=$message['latitude']; 
                     date_default_timezone_set('Asia/Taipei');$time=date("Y-m-d H:i:s");
-		    if($m_message!="" && $userid!='Ud9a4e29db28b8b07a78cecf6d8ec3bdb'){
+		    if($m_message!="" && $userid!='Ud9a4e29db28b8b07a78cecf6d8ec3bdb' && $roomid!='R8466f385da9bd8eac6fb509622c0a892'){
 		    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($m_message);
 		    $response = $bot->pushMessage('R8466f385da9bd8eac6fb509622c0a892', $textMessageBuilder);
 		    }    
