9e42ae06cb178d5e7e9cf5dd2d8c7bc6fa357d73
chouyuhang <a103410102a@gmail.com>
2018-03-28 19:36:42 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -83,11 +83,14 @@ foreach ($client->parseEvents() as $event) {
                     date_default_timezone_set('Asia/Taipei');$time=date("Y-m-d H:i:s");
 		    //if($m_message!="" && $userid!='Ud9a4e29db28b8b07a78cecf6d8ec3bdb' && $roomid!='R8466f385da9bd8eac6fb509622c0a892'){
 	            if($m_message!=""){
-		    $mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
-		    $sql = "SELECT userid from mysql";
-		    $result = $mysqli->query($sql);
-		    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($m_message);
-		    $response = $bot->pushMessage($sql, $textMessageBuilder);
+		    	$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
+		    	$sql = "SELECT userid from mysql";
+		    	$result = $mysqli->query($sql);
+			while($row = $result->fetch_array(MYSQLI_BOTH)) {
+  				$user = $row['userid'] ;
+ 			 }
+		    	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($m_message);
+		    	$response = $bot->pushMessage($user, $textMessageBuilder);
 		    }    
 		    else if($m_message=="進"){
 			$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
