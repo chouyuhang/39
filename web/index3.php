7bbfafdb9a9f105b2c8378ff5ada6b3d975bdd20
chouyuhang <a103410102a@gmail.com>
2018-03-31 19:52:48 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -57,7 +57,7 @@ foreach ($client->parseEvents() as $event) {
 			$row = $result->fetch_array(MYSQLI_BOTH);
 			    $worktime = $row['worktime'] ;
 			    $tim=strtotime($worktime."+1 min");
-			if($m_message=="" && $time==$tim){
+			if($m_message==""){
 			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("請按進出按鈕");
 		    	$response = $bot->pushMessage('Ub28a7054f2aa2bfeeb103fb53ca35f32', $textMessageBuilder);
 		        sleep(10);
