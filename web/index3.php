a1b2e665326832bb7efe3277d64378af9886cc59
chouyuhang <a103410102a@gmail.com>
2018-04-05 19:35:29 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -48,7 +48,6 @@ foreach ($client->parseEvents() as $event) {
 			$number=$number+1;
 			$sql="INSERT INTO mysql (number,name,userid,worktime,location,longitude,latitude) VALUES ('$number','$displayName','$userid','$time','$address','$longitude','$latitude')";
 			$result = $mysqli->query($sql);
-			if($type=="template"){
 			$client->replyMessage(array(
   			'replyToken' => $event['replyToken'],
     			'messages' => array(
@@ -82,8 +81,9 @@ foreach ($client->parseEvents() as $event) {
 				
  			 }
 				$tim=date('Y-m-d H:i:s', strtotime($worktime,"+5 seconds"));*/
+			if($type=="template"){
 			sleep(3);
-			if($m_message=="" && ($m_message!="進"||$m_message!="出")){
+			if($m_message=="" || ($m_message!="進"||$m_message!="出")){
 				$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("請按進出按鈕");
 		    		$response = $bot->pushMessage('Ub28a7054f2aa2bfeeb103fb53ca35f32', $textMessageBuilder);
 			}
