e146011bfe65ff7c0f7bcfc4714417b1bb473b11
chouyuhang <a103410102a@gmail.com>
2018-04-08 19:24:59 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -74,7 +74,7 @@ foreach ($client->parseEvents() as $event) {
 			while($row = $result->fetch_array(MYSQLI_BOTH)) {
   				$name = $row['name'] ;
  			 }
-			if($name!="")){
+			if($name!=""){
 				$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("請按進出按鈕");
 		    		$response = $bot->pushMessage('Ub28a7054f2aa2bfeeb103fb53ca35f32', $textMessageBuilder);
 			}
