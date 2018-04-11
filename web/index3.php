6fef41d59a8a882447730f79bb7245ed9fbe4e33
chouyuhang <a103410102a@gmail.com>
2018-04-11 21:56:26 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -131,9 +131,9 @@ foreach ($client->parseEvents() as $event) {
 			$index=0;
 			$sql = "SELECT inside from inin";
 			$result = $mysqli->query($sql);
-			$row = $result->fetch_array(MYSQLI_BOTH))
-  				$inside = $row['inside'] ;
-			
+			$row = $result->fetch_array(MYSQLI_BOTH)
+  				$inside[$index] = $row['inside'] ;
+
 			//$sql = "SELECT inside from inin where inside like '%$m_message%'";
 			//$result = $mysqli->query($sql);
 			//while($row = $result->fetch_array(MYSQLI_BOTH)){
