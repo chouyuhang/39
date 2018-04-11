bb5cb89e65b600cc167b3370f7fbe1dc5806463d
chouyuhang <a103410102a@gmail.com>
2018-04-11 15:38:29 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -133,7 +133,7 @@ foreach ($client->parseEvents() as $event) {
 			while($row = $result->fetch_array(MYSQLI_BOTH)) {
   				$inside = $row['inside'] ;
  			 }
-		    if(preg_match("/$inside/in","$m_message")){
+		    if(preg_match("/$inside/i","$m_message")){
 			$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
 			$sql = "SELECT location from mysql where worktype='' and userid='$userid'";
 			$result = $mysqli->query($sql);
