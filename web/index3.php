74a09d77352dcabd0d5727eb8ffc9d85bf320116
chouyuhang <a103410102a@gmail.com>
2018-04-11 17:03:46 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -168,6 +168,11 @@ foreach ($client->parseEvents() as $event) {
 		    	$response = $bot->pushMessage($userid, $textMessageBuilder);
 			}
 		    }
+		    $mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
+			$sql = "SELECT outside from outout";
+			$result = $mysqli->query($sql);
+			$row = $result->fetch_array(MYSQLI_BOTH);
+  				$outside = $row['outside'] ;
 		    if(preg_match("/$outside/i","$m_message")){
 			$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
 			$sql = "SELECT location from mysql where worktype='' and userid='$userid'";
