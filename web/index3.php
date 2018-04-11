cbb3926ab41c3c21eb455bb3924ac2219dd5c788
chouyuhang <a103410102a@gmail.com>
2018-04-11 21:52:25 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -128,12 +128,19 @@ foreach ($client->parseEvents() as $event) {
 			$response = $bot->pushMessage('Ud9a4e29db28b8b07a78cecf6d8ec3bdb', $textMessageBuilder);
 		    }*/
 		    $mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
+			$index=0;
 			$sql = "SELECT inside from inin";
 			$result = $mysqli->query($sql);
-			$row = $result->fetch_array(MYSQLI_BOTH);
-  				$inside = $row['inside'] ;
-			
-		    if(preg_match("/$inside/","$m_message")){
+			while($row = $result->fetch_array(MYSQLI_BOTH)){
+  				$inside[$index] = $row['inside'] ;
+				$index++;
+			}
+			//$sql = "SELECT inside from inin where inside like '%$m_message%'";
+			//$result = $mysqli->query($sql);
+			//while($row = $result->fetch_array(MYSQLI_BOTH)){
+				
+			//}
+		    if(preg_match("/$inside/i","$m_message")){
 			$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
 			$sql = "SELECT location from mysql where worktype='' and userid='$userid'";
 			$result = $mysqli->query($sql);
@@ -173,7 +180,7 @@ foreach ($client->parseEvents() as $event) {
 			$result = $mysqli->query($sql);
 			$row = $result->fetch_array(MYSQLI_BOTH);
   				$outside = $row['outside'] ;
-		   else if(preg_match("/$outside/","$m_message")){
+		   else if(preg_match("/$outside/i","$m_message")){
 			$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
 			$sql = "SELECT location from mysql where worktype='' and userid='$userid'";
 			$result = $mysqli->query($sql);
