53b6aae0dc15539a3702b4a4ecd7977a6653376b
chouyuhang <a103410102a@gmail.com>
2018-04-11 17:01:04 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -128,7 +128,7 @@ foreach ($client->parseEvents() as $event) {
 			$response = $bot->pushMessage('Ud9a4e29db28b8b07a78cecf6d8ec3bdb', $textMessageBuilder);
 		    }*/
 		    $mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
-			$sql = "SELECT  from inin";
+			$sql = "SELECT inside from inin";
 			$result = $mysqli->query($sql);
 			$row = $result->fetch_array(MYSQLI_BOTH);
   				$inside = $row['inside'] ;
