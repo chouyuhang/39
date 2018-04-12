c43626599aa3d4fc0cf7311e3f8ee798aed1c2c7
chouyuhang <a103410102a@gmail.com>
2018-04-12 17:59:40 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -113,7 +113,8 @@ foreach ($client->parseEvents() as $event) {
 			if($location==""){
 				$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("請定位你的位置");
 				$response = $bot->pushMessage($userId, $textMessageBuilder);
-			} 
+			}
+		     }
 		 }
 		break;
 		case 'text':
