20bdb1e94869101f7cc1e340d5b128d250cb6bf7
chouyuhang <a103410102a@gmail.com>
2018-04-12 11:31:37 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -251,13 +251,13 @@ foreach ($client->parseEvents() as $event) {
                                      array(
                                     'type' => 'postback',
                                     'label' => 'IN',
-				    "data": "action=in&itemid=12",
+				    'data' => 'action=in&itemid=12',
                                     'text' => '設置成功'
                                 ),
                                     array(
                                     'type' => 'postback',
                                     'label' => 'OUT',
-				    "data": "action=out&itemid=123",
+				    'data' => 'action=out&itemid=123',
                                     'text' => '設置成功'
                                  ),
                                     array(
