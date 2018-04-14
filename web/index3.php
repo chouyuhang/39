fa52ec33c34051645a5af383e1c3789c1d4268d1
chouyuhang <a103410102a@gmail.com>
2018-04-14 20:57:30 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -289,7 +289,7 @@ foreach ($client->parseEvents() as $event) {
 		    	$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
 			$sql = "select max(name),worktime from mysql where worktype='進'";
 			$result = $mysqli->query($sql);
-			/*while(*/$row = $result->fetch_array(MYSQLI_BOTH) {
+			/*while(*/$row = $result->fetch_array(MYSQLI_BOTH) 
 				$name = $row['name'];
 				$worktime = $row['worktime'];
 			//}
