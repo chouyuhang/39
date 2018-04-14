2514703e5147bfb02474e017bab4eefb0f3e34fd
chouyuhang <a103410102a@gmail.com>
2018-04-14 21:05:26 +0800

    Update index3.php

--- web/index3.php
+++ web/index3.php
@@ -287,7 +287,7 @@ foreach ($client->parseEvents() as $event) {
 			}
 		    }else if($m_message=="查"){
 		    	$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
-			$sql = "select max(name),worktime from mysql where worktype='進' and worktime='$date'";
+			$sql = "select max(name),worktime from mysql where worktype='進'";
 			$result = $mysqli->query($sql);
 			while($row = $result->fetch_array(MYSQLI_BOTH)){ 
 				$name = $row['name'];
