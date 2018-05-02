bfefa7847d53f51d4f05e7d6b33aead738efe655
chouyuhang <a103410102a@gmail.com>
2018-05-02 19:27:57 +0800

    Update index4.php

--- web/index4.php
+++ web/index4.php
@@ -24,4 +24,34 @@ $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
+function randomkeys($length){
+$pattern = "1234567890abcdefghijklmnopqrstuvwxyz";
+for($i=0;$i<$length;$i++){
+$key .= $pattern{rand(0,35)};
+}
+return $key;
+}
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $replyToken=$event['replyToken'];
+                	$m_message = $message['text']; $source=$event['source']; $idtype = $source['type'];  $id=$source['userId'];
+                    $roomid=$source['roomId']; $groupid=$source['groupId'];
+                    $pictureUrl=$message['pictureUrl'];$res = $bot->getProfile($id);$profile = $res->getJSONDecodedBody();
+                    $displayName = $profile['displayName'];
+                    date_default_timezone_set('Asia/Taipei');
+                    if($m_message=="安安"){
+                        $msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder(randomkeys(8));
+                        $bot->replyMessage($replyToken,$msg);
+                    }
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
